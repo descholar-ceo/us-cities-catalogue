5f447ea4e8debca42141d681d81ac6da54e8b66b
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-22 20:27:57 +0200

    Add DISPLAY_DETAILS_OF_A_CITY action type

--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,3 +1,4 @@
 export const GET_ALL_CITIES = 'GET_ALL_CITIES';
 export const FILTER_CITY = 'FILTER_CITY';
+export const DISPLAY_DETAILS_OF_A_CITY = 'DISPLAY_DETAILS_OF_A_CITY';
 export const GET_ERROR = 'GET_ERROR';
