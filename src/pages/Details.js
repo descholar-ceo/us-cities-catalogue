8079bd2b1664dd103160be7ecce5662dfc7e402e
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-23 00:28:20 +0200

    Destructure all city's properties

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,11 +4,23 @@ import { connect } from 'react-redux';
 const Details = ({ cities, match: { params } }) => {
   const { city: selectedCity } = params;
   const myCity = cities.filter(city => city.city === selectedCity);
-  const { city, population } = myCity[0];
+  const {
+    city,
+    population,
+    growth_from_2000_to_2013: growthFrom2000To2013,
+    latitude,
+    longitude,
+    rank,
+    state,
+  } = myCity[0];
   return (
     <div>
       <h1>{city}</h1>
       <h2>{population}</h2>
+      <h3>{growthFrom2000To2013}</h3>
+      <h4>{`(${latitude}, ${longitude})`}</h4>
+      <h5>{rank}</h5>
+      <h6>{state}</h6>
     </div>
   );
 };
