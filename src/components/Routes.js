a46457d6add0f64b59fc298b10fd0ea99640e567
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-22 20:24:57 +0200

    Add city details to destructirin

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,22 +3,10 @@ import Details from '../pages/Details';
 import Home from '../pages/Home';
 import Fourofourpage from '../pages/Fourofourpage';
 
-// const city = {
-//   city: 'Columbus',
-//   growth_from_2000_to_2013: '14.8%',
-//   latitude: 39.9611755,
-//   longitude: -82.99879419999999,
-//   population: '822553',
-//   rank: '15',
-//   state: 'Ohio',
-// };
-
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/details">
-      <Details />
-    </Route>
+    <Route exact path="/details" component={Details} />
     <Route component={Fourofourpage} />
   </Switch>
 );
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,9 +1,22 @@
-const Details = props => {
-  // const { city: mCity } = city;
-  console.log({ props });
+import PropTypes from 'prop-types';
+
+const Details = ({ city }) => {
+  const { city: mCity } = city;
   return (
-    <h1>mCity</h1>
+    <h1>{mCity}</h1>
   );
+};
+
+Details.propTypes = {
+  city: PropTypes.shape({
+    city: PropTypes.string,
+    population: PropTypes.string,
+    growth_from_2000_to_2013: PropTypes.string,
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    rank: PropTypes.string,
+    state: PropTypes.string,
+  }).isRequired,
 };
 
 export default Details;
