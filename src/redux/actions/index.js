65eadba6f4891dc9a44eb59084618385faf90d05
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-22 20:29:16 +0200

    Add display city action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FILTER_CITY, GET_ALL_CITIES } from './actionsTypes';
+import { DISPLAY_DETAILS_OF_A_CITY, GET_ALL_CITIES } from './actionsTypes';
 
 export const getCitiesAction = () => dispatch => {
   const sampleCitiesURL = 'https://gist.githubusercontent.com/descholar-ceo/9b11200f0a83417cbdbd2057afbdf855/raw/f024c7a831ac266ce0dea53683853daf4b6a6ab5/us-cities.json';
@@ -13,7 +13,7 @@ export const getCitiesAction = () => dispatch => {
     .catch(err => err.response.data);
 };
 
-export const filterCity = filter => ({
-  type: FILTER_CITY,
-  filter,
+export const filterCity = city => ({
+  type: DISPLAY_DETAILS_OF_A_CITY,
+  city,
 });
