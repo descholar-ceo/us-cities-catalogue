0fa3c6783767f05901efe44d9263fee45a3ff576
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-22 16:16:48 +0200

    Display a sample bg

--- src/components/City.js
+++ src/components/City.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import sampleBg from '../assets/images/washinton-dc-pic.jpg';
 
 const City = ({ city }) => {
   const { city: mCity, population } = city;
   return (
     <div>
+      <img src={sampleBg} alt="washington" />
       <h1>{mCity}</h1>
       <h2>{population}</h2>
     </div>
