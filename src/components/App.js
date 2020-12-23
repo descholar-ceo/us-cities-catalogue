0d662c53c02125c0b8057e78a923d1399f99516d
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-23 08:13:32 +0200

    Render navbar in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes />
     </BrowserRouter>
   );
