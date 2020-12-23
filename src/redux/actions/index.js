9c34a39cc152fd09054d3e99f6f34bafee54adfe
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-23 10:02:49 +0200

    Implement redux for filtering cities

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { DISPLAY_DETAILS_OF_A_CITY, FILTER_CITY, GET_ALL_CITIES } from './actionsTypes';
+import { FILTER_CITY, GET_ALL_CITIES } from './actionsTypes';
 
 export const getCitiesAction = () => dispatch => {
   const sampleCitiesURL = 'https://gist.githubusercontent.com/descholar-ceo/9b11200f0a83417cbdbd2057afbdf855/raw/f024c7a831ac266ce0dea53683853daf4b6a6ab5/us-cities.json';
@@ -16,9 +16,4 @@ export const getCitiesAction = () => dispatch => {
 export const filterCity = mFilter => ({
   mFilter,
   type: FILTER_CITY,
-});
-
-export const displayOneCity = city => dispatch => dispatch({
-  type: DISPLAY_DETAILS_OF_A_CITY,
-  city,
 });
