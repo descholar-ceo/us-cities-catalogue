33b8dfa3667b7528223746b29d108e06047db0f7
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-23 19:45:42 +0200

    Filter city usin regex

--- src/containers/AllCities.js
+++ src/containers/AllCities.js
@@ -2,8 +2,15 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import City from '../components/City';
 
-const AllCities = ({ cities }) => {
-  const cityRows = cities.map((currCity, index) => <City key={`city-num${index + 1}`} city={currCity} />);
+const AllCities = ({ cities, filter }) => {
+  const cityToDisplay = () => {
+    if (filter === 'All') {
+      return cities;
+    }
+    const regex = new RegExp(`^${filter}`, 'gi');
+    return cities.filter(currCity => currCity.city.match(regex) || currCity.state.match(regex));
+  };
+  const cityRows = cityToDisplay().map((currCity, index) => <City key={`city-num${index + 1}`} city={currCity} />);
   return (
     <div className="global-container">
       {cityRows}
@@ -13,8 +20,9 @@ const AllCities = ({ cities }) => {
 
 AllCities.propTypes = {
   cities: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = state => ({ cities: state.cities });
+const mapStateToProps = state => ({ cities: state.cities, filter: state.filter });
 
 export default connect(mapStateToProps, null)(AllCities);
