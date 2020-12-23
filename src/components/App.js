fa550f199f443e10f456523b908f20b5c6d3348f
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-23 19:53:55 +0200

    Remove navbar from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
-import NavBar from './NavBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <Routes />
     </BrowserRouter>
   );
