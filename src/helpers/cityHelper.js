fcc700fcaa7103b3860016233c98fd6542be5856
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-24 18:30:33 +0200

    Remove commented codes and conle.log statements

--- src/helpers/cityHelper.js
+++ src/helpers/cityHelper.js
@@ -7,19 +7,10 @@ const getCityData = async city => {
   try {
     const cityData = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${cityToSend}/images`);
     res = cityData.data.photos[0].image.mobile;
-    console.log({ res });
   } catch (err) {
     res = fallbackImg;
-    console.log(err.response.data);
   }
   return res;
-
-  // const cityData = fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCmKsMOokFjKKOQ5iBPIscE0-u234aNxMs`, { mode: 'no-cors' })
-  //   .then(response => (response.json())).then(res => {
-  //     console.log(res);
-  //     return res;
-  //   });
-  // return cityData;
 };
 
 export default getCityData;
