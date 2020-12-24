5c89df660dc025e2a9aefc67cf09cc2335136fac
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-24 21:53:04 +0200

    Remove unnecessary reducer

--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,4 +1,3 @@
 export const GET_ALL_CITIES = 'GET_ALL_CITIES';
 export const FILTER_CITY = 'FILTER_CITY';
-export const GET_CITY_IMAGE = 'GET_CITY_IMAGE';
 export const GET_ERROR = 'GET_ERROR';
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import cityReducer from './city';
-import cityImageReducer from './cityImage';
 import filterReducer from './filter';
 
 export default combineReducers({
   cityReducer,
   filter: filterReducer,
-  cityImageReducer,
 });
