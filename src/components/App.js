2028541f277664b7bd1209a25d3d35b29d483a3b
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-24 21:35:36 +0200

    Remove connect from the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import Routes from './Routes';
-import { getCitiesAction } from '../redux/actions';
 
-const App = ({ getCitiesAction }) => {
-  useEffect(() => {
-    getCitiesAction();
-  }, []);
-  return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
-App.propTypes = {
-  getCitiesAction: PropTypes.func.isRequired,
-};
-
-export default connect(null, { getCitiesAction })(App);
+export default App;
