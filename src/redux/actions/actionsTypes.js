96b78099f81031451868eb3050433b6d46e8172f
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-24 18:32:46 +0200

    Create actiontype of getcityimage

--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,4 +1,4 @@
 export const GET_ALL_CITIES = 'GET_ALL_CITIES';
 export const FILTER_CITY = 'FILTER_CITY';
-export const DISPLAY_DETAILS_OF_A_CITY = 'DISPLAY_DETAILS_OF_A_CITY';
+export const GET_CITY_IMAGE = 'GET_CITY_IMAGE';
 export const GET_ERROR = 'GET_ERROR';
