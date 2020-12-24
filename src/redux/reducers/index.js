dd835a1254269b7c6cef57f30bc29ac7970134b3
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-24 18:48:07 +0200

    Add cityImageReducer to combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 import cityReducer from './city';
+import cityImageReducer from './cityImage';
 import filterReducer from './filter';
 
-export default combineReducers({ cityReducer, filter: filterReducer });
+export default combineReducers({
+  cityReducer,
+  filter: filterReducer,
+  cityImageReducer,
+});
