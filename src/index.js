6df3692ff2ae56569b05bc8891748c58e30ac3e5
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-25 20:07:32 +0200

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import './assets/styles/styles.scss';
 import store from './redux/configureStore';
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
