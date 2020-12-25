35517619e6b0d7345d27d982eefcf7dbdc8234dd
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-25 10:40:40 +0200

    Add test skeleton of the test getCities action

--- src/__tests__/redux/actions.test.js
+++ src/__tests__/redux/actions.test.js
@@ -1,13 +1,19 @@
 import { filterCity } from '../../redux/actions';
 import { FILTER_CITY } from '../../redux/actions/actionsTypes';
 
-describe('Test filterCity actions', () => {
+describe('Test filterCity action', () => {
   it('The filter action returns the filterString and actiontype', done => {
     const res = filterCity('Chicago');
     expect(res).toHaveProperty('type');
     expect(res.type).toEqual(FILTER_CITY);
     expect(res).toHaveProperty('mFilter');
     expect(res.mFilter).toEqual('Chicago');
+    done();
+  });
+});
+
+describe('Test getCitiesAction action', () => {
+  it('The getCities action dispatches cities after fetching them from the API', done => {
     done();
   });
 });
