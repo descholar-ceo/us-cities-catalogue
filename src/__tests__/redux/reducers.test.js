9716df2a971929184cb8795b6876a39a8e621241
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-25 12:05:37 +0200

    Add test of a custom state passed

--- src/__tests__/redux/reducers.test.js
+++ src/__tests__/redux/reducers.test.js
@@ -1,4 +1,5 @@
-const { default: filterReducer } = require('../../redux/reducers/filter');
+import { filterCity } from '../../redux/actions';
+import filterReducer from '../../redux/reducers/filter';
 
 describe('Test of filterReducer', () => {
   it('filterReduce returns initialState when action return undefined', done => {
@@ -8,6 +9,10 @@ describe('Test of filterReducer', () => {
   });
 
   it('filterReducer handles FILTER_CITY action', done => {
+    const res = filterReducer(undefined, filterCity);
+    const res2 = filterReducer({ filter: 'Chicago' }, filterCity);
+    expect(res).toEqual('All');
+    expect(res2).toEqual({ filter: 'Chicago' });
     done();
   });
 });
