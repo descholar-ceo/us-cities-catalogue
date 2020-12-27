7a0e0cc31e7a0e530c459eb978fa07d78804df69
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-27 11:40:10 +0200

    Add test for route component

--- src/__tests__/components/Routes.test.js
+++ src/__tests__/components/Routes.test.js
@@ -1,20 +1,11 @@
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../../components/Routes';
 
 describe('Test Routes component', () => {
   it('Renders the Routes component', done => {
     const RoutesComp = shallow(<BrowserRouter><Routes /></BrowserRouter>);
-    expect(RoutesComp.containsAllMatchingElements(<Routes />)).toBeTruthy();
-    done();
-  });
-});
-
-describe('Test the snapshot of Routes component', () => {
-  it('Render the correct snapshot', done => {
-    const RoutesSnapshot = renderer.create(<BrowserRouter><Routes /></BrowserRouter>).toJSON();
-    expect(RoutesSnapshot).toMatchSnapshot();
+    expect(RoutesComp.containsMatchingElement(<Routes />)).toBeTruthy();
     done();
   });
 });
