ab9c9738f40c86334d372dfca94cefe7197c7b76
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-27 14:26:03 +0200

    Add assertion to urlgenerator test

--- src/__tests__/helpers/wikipediaUrlGenerator.test.js
+++ src/__tests__/helpers/wikipediaUrlGenerator.test.js
@@ -1,5 +1,9 @@
+import WikipediaUrlGenerator from '../../helpers/wikipediaUrlGenerator';
+
 describe('Test for wikipediaUrlGenerator', () => {
   it('Returns the url to wikipedia depending on the passed city', done => {
+    const generatedUrl = WikipediaUrlGenerator('chicago');
+    expect(generatedUrl).toEqual('https://en.wikipedia.org/wiki/chicago');
     done();
   });
 });
