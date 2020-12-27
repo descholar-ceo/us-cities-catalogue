6d41edd6dbbc834abc5f5c380c6a3d9e9a1b4568
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-27 10:38:41 +0200

    Add test of the form

--- src/__tests__/components/SearchForm.test.js
+++ src/__tests__/components/SearchForm.test.js
@@ -1,11 +1,20 @@
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
 import SearchForm from '../../components/SearchForm';
 
+const initialState = {};
+const mockStore = configureStore();
+let wrapper;
+let store;
+
 describe('Test SearchForm component', () => {
+  beforeEach(() => {
+    store = mockStore(initialState);
+    wrapper = renderer.create(<SearchForm store={store} />);
+  });
   it('Renders the SearchForm component', done => {
-    const SearchFormComp = shallow(<SearchForm />);
-    expect(SearchFormComp.type()).toBe('div');
-    expect(SearchFormComp.find('div.loader').exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
     done();
   });
 });
